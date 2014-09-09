5a2c8d376642af8d40661c5b3ae06837c81feb62
shyaken <shyaken@live.com>
2014-09-10 02:37:17 +0700

    update

--- application/controllers/crawl.php
+++ application/controllers/crawl.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class crawler extends CI_Controller{
+class Crawl extends CI_Controller{
 
 	/**
 	 * Index Page for this controller.
