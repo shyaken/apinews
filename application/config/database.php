bab4d830048a4e18517c6e0384a85b3a8e13539a
shyaken <shyaken@live.com>
2014-09-10 02:35:11 +0700

    update

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['username'] = 'apinews';
+$db['default']['password'] = 'apinewspass';
 $db['default']['database'] = 'apinews';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
