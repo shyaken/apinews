5c1f75f1c588bafb02ac0439aee365ba174969ec
Chung Hd <shyaken@live.com>
2014-09-15 14:47:13 +0700

    update get detail

--- application/controllers/apio.php
+++ application/controllers/apio.php
@@ -212,8 +212,7 @@ class Apio extends CI_Controller {
 		}
 		$query = $this->db->get_where('records',array('id' => $param['id']));
 		$record = $query->first_row();
-		print_r($record);
-		if($query->num_rows() === 0 || !isset($record['html_content'])) {
+		if($query->num_rows() < 1 || !isset($record['html_content'])) {
 			die("fuck");
 			$response = array (
 				'status' => false,
